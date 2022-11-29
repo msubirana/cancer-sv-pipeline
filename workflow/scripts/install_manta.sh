424e3ad303869b21d067203f8671c8842261fde6
mb2subi@gmail.com <mb2subi@gmail.com>
2022-11-29 10:59:20 +0100

    finished manta rule and added delly rule

--- workflow/scripts/install_manta.sh
+++ workflow/scripts/install_manta.sh
@@ -1,11 +1,9 @@
 #!/bin/env bash
 set -u
 install_dir=$1
-outdir=$2
-
+manta_version=$2
 # TODO make version a input parameter
-manta_version=1.6.0
-wget https://github.com/Illumina/manta/releases/download/v$manta_version/manta-$manta_version.centos6_x86_64.tar.bz2 -O $outdir/manta.tar.bz2
-tar -xjvf $outdir/manta.tar.bz2  && 
-    rm $outdir/manta.tar.bz2 &&
-    mv manta-$manta_version.centos6_x86_64 $install_dir
+
+wget https://github.com/Illumina/manta/releases/download/v$manta_version/manta-$manta_version.centos6_x86_64.tar.bz2 -O $install_dir/manta.tar.bz2
+tar -xjvf $install_dir/manta.tar.bz2 -C $install_dir && 
+    rm $install_dir/manta.tar.bz2
