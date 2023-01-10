b6994978d4052b55b24f2e4e12a0796038bb880c
mb2subi@gmail.com <mb2subi@gmail.com>
2023-01-10 22:04:06 +0100

    gridds pipe small changes

--- workflow/scripts/install_gridss.sh
+++ workflow/scripts/install_gridss.sh
@@ -4,7 +4,6 @@ install_dir=$1
 gridss_version=$2
 # TODO make version a input parameter
 
-install_dir=${install_dir}/gridss
-mkdir $install_dir
 wget https://github.com/PapenfussLab/gridss/releases/download/v$gridss_version/gridss-$gridss_version-gridss-jar-with-dependencies.jar -O $install_dir/gridss-$gridss_version-gridss-jar-with-dependencies.jar
-wget https://github.com/PapenfussLab/gridss/blob/master/example/ENCFF356LFX.bed -O $install_dir/exclude_list.bed
+wget https://raw.githubusercontent.com/PapenfussLab/gridss/master/example/ENCFF356LFX.bed -O $install_dir/exclude_list.bed
+
